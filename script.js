e3169778a098e64be4e238965d1e60fcb51e2ae5
deax247 <abylay.o@mail.ru>
2025-10-25 19:40:21 +0500

    finished the project

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let humanScore = 0
-let computerScore = 0 
-
 function getComputerChoice() {
-    let x = (floor(Math.random() * 2)) + 1;
+    let x = Math.floor(Math.random() * 3 + 1);
 
     if (x == 1) {
         return "rock"
@@ -20,12 +17,66 @@ function getHumanChoice() {
 }
 
 
-function playRound(humanChoice , computerChoice) {
-    humanChoice = humanChoice.toLowerCase()
-    if humanChoice != "rock" || humanChoice != "paper" || humanChoice != "scissors" {
-        alert("wrong input")
-        return
+
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound() {
+        let hc = getHumanChoice()
+        let cc = getComputerChoice()
+
+        hc = hc.toLowerCase()
+        if (hc != "rock" && hc != "paper" && hc != "scissors") {
+            alert("wrong input")
+            return
+        }
+
+        if (hc == "rock" && cc == "rock") {
+            console.log("It's a tie!");
+        } else if (hc == "rock" && cc == "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerScore++
+        } else if (hc == "rock" && cc == "scissors") {
+            console.log("You win! Rock beats Scissors");
+            humanScore++
+        } else if (hc == "paper" && cc == "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++
+        } else if (hc == "paper" && cc == "paper") {
+            console.log("It's a tie!");
+        } else if (hc == "paper" && cc == "scissors") {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++
+        } else if (hc == "scissors" && cc == "rock") {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++
+        } else if (hc == "scissors" && cc == "paper") {
+            console.log("You win! Scissors beats Paper");
+            humanScore++
+        } else if (hc == "scissors" && cc == "scissors") {
+            console.log("It's a tie!");
+        } else {
+            alert("Smth's off...")
+        }
+        console.log("Human score: " + humanScore + "\nComputer score: " + computerScore);
     }
 
-    let humanChoice
+    while (humanScore < 3 && computerScore < 3) {
+        playRound();
+    }
+
+    if (humanScore === 3) {
+        console.log("🎉 You won the game!");
+    } else {
+        console.log("💻 Computer won the game!");
+    }
+
+
 }
+
+
+playGame();
+
